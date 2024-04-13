2651427a1df13e06807c82c6d1effc486941c25b
zeqingwang <61963896+zeqingwang@users.noreply.github.com>
2024-04-13 03:09:15 -0700

    update

--- la_crime/src/App.js
+++ la_crime/src/App.js
@@ -4,26 +4,68 @@ import MapComponent from './components/MapComponent';
 
 function App() {
   const [filters, setFilters] = useState({
-    dateOccurred: '2010',
+    dateOccurred: '',
     areaName: '',
     crimeCode: '',
-    latitude: '',
-    longitude: ''
+    violentLevel: '',
+    latitude: -118.2695,
+    longitude: 33.9825,
+    radius: 10
   });
+
   const [data, setData] = useState([]);
 
-  const onSearch = () => {
+  const onSearch = async () => {
     // Fetch data based on filters
     // For demonstration, we're just setting random data
-    setData([
-      { latitude: parseFloat(filters.latitude) || 0, longitude: parseFloat(filters.longitude) || 0 }
-    ]);
+    //const url = 'http://lacrimeexplorer-env.eba-mw97embs.us-west-1.elasticbeanstalk.com/search-crimes';
+    const url = '/LACrimeAnalysisTool(2)/search-crimes'
+
+    const querybody = {
+      // longitude: filters.longitude,
+      // latitude: filters.latitude,
+      // radius: filters.radius
+      "startDate": "2010-01-01",
+      "endDate": "2023-01-01",
+      "crimeCode": "900",
+      "longitude": -118.2695,
+      "latitude": 33.9825,
+      "radius": 0.625
+
+    };
+    try {
+      // Making a POST request with Fetch API
+      //console.log(filters);
+      console.log(querybody);
+      const response = await fetch(url,
+        {
+          method: 'POST',
+          mode: 'no-cors',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(querybody)
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+      // Update the state with the fetched data
+      setData(result);
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+    }
+
   };
 
   return (
     <div className="App">
       <FilterComponent filters={filters} setFilters={setFilters} onSearch={onSearch} />
-      <MapComponent data={data} />
+      <MapComponent data={data} center={[filters.latitude, filters.longitude]} />
     </div>
   );
 }
--- la_crime/src/components/MapComponent.js
+++ la_crime/src/components/MapComponent.js
@@ -5,14 +5,16 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 const containerStyle = { width: '400px', height: '400px' };
 
 
-const MapComponent = ({ data }) => (
+const MapComponent = ({ data, center }) => (
     <LoadScript googleMapsApiKey="AIzaSyBw03WM4TYrA1powE4pKdMIHN_clays-nU">
         <GoogleMap
             mapContainerStyle={containerStyle}
-            center={{ lat: data.length ? data[0].latitude : -34.397, lng: data.length ? data[0].longitude : 150.644 }}
+            //center={{ lat: data.length ? data[0].latitude : -34.397, lng: data.length ? data[0].longitude : 150.644 }}
+            center={{ lat: center[0], lng: center[1] }}
             zoom={10}
         >
             {data.map((item, index) => (
+
                 <Marker key={index} position={{ lat: item.latitude, lng: item.longitude }} />
             ))}
         </GoogleMap>
