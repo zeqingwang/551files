6f00f5111d077790c070cac2008c17082c9702d2
zeqingwang <61963896+zeqingwang@users.noreply.github.com>
2024-04-13 23:32:41 -0700

    change the detail to html

--- la_crime/src/components/MapComponent.js
+++ la_crime/src/components/MapComponent.js
@@ -5,32 +5,66 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 const containerStyle = { width: '400px', height: '400px' };
 const handleMarkerClick = async (drNo) => {
 
-    const url = '/LACrimeAnalysisTool(2)/crimeById?drNo=' + drNo
-    console.log(url)
+    // const url = '/LACrimeAnalysisTool(2)/crimeById'
+    // console.log(url)
+
+    // try {
+    //     // Making a POST request with Fetch API
+    //     const querybody = {
+
+
+    //         "drNo": drNo
+
+    //     };
+    //     const response = await fetch(url,
+    //         {
+    //             method: 'POST',
+    //             mode: 'no-cors',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify(querybody)
+    //         }
+    //     );
+    //     console.log(response);
+    //     if (!response.ok) {
+    //         throw new Error(`HTTP error! status: ${response.status}`);
+    //     }
+
+    //     const result = await response.json();
+    //     console.log(result);
+
+    // } catch (error) {
+    //     console.error('Failed to fetch data:', error);
+    // }
+    const detailurl = `/LACrimeAnalysisTool(2)/crimeById?drNo=` + drNo;
+
+    //console.log(url);
 
     try {
-        // Making a POST request with Fetch API
-
-        const response = await fetch(url,
-            {
-                method: 'POST',
-                mode: 'no-cors',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                //body: JSON.stringify(querybody)
+        // Making a GET request with Fetch API
+        const response = await fetch(detailurl, {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'text/html'
             }
-        );
-
+        });
+        console.log(response);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const result = await response.json();
-        console.log(result);
-        // Update the state with the fetched data
-        //setCenter([filters.latitude, filters.longitude]);
-        //setData(result);
+        // Assuming the response is direct HTML content
+        const resultHtml = await response.text();
+        console.log(resultHtml);
+
+        // Use a blob to create a URL for the HTML content
+        const blob = new Blob([resultHtml], { type: 'text/html' });
+        const url = URL.createObjectURL(blob);
+
+        // Open the result in a new tab
+        window.open(url, '_blank');
     } catch (error) {
         console.error('Failed to fetch data:', error);
     }
